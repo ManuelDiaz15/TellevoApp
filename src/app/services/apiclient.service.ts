9b90cdbc230e3964d83231fd1746fb4cb7244c8c
Manuel Diaz <Mana.diaz@duocuc.cl>
2021-11-05 19:04:37 -0300

    Agregando 2°

--- src/app/services/apiclient.service.ts
+++ src/app/services/apiclient.service.ts
@@ -5,8 +5,17 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class APIClientService {
- 
 
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    })
+  }
+
+  // Se establece la base url del API a consumir
+ // apiURL = 'https://jsonplaceholder.typicode.com';
+ apiURL = 'http://192.168.0.3:30000'
 
   constructor() { }
 }
