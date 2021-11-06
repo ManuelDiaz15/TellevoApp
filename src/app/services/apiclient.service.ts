dfaac4c0142649e8435e2eb33e7c72123a477550
Manuel Diaz <Mana.diaz@duocuc.cl>
2021-11-05 22:08:33 -0300

    Manu

--- src/app/services/apiclient.service.ts
+++ src/app/services/apiclient.service.ts
@@ -1,5 +1,7 @@
-import { HttpHeaders } from '@angular/common/http';
+import { HttpHeaders, HttpClientModule, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,8 +16,47 @@ export class APIClientService {
   }
 
   // Se establece la base url del API a consumir
- // apiURL = 'https://jsonplaceholder.typicode.com';
- apiURL = 'http://192.168.0.3:30000'
+  // apiURL = 'https://jsonplaceholder.typicode.com';
+  apiURL = 'http://192.168.0.3:30000'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUsuarios(): Observable<any> {
+    return this.http.get(this.apiURL + '/users/').pipe(
+      retry(3)
+    );
+  }
+  getUsuario(userId): Observable<any> {
+    return this.http.get(this.apiURL + '/users/' + userId).pipe(
+      retry(3)
+    );
+  }
+
+  getPost(id): Observable<any> {
+    return this.http.get(this.apiURL + '/posts/'+id).pipe(
+      retry(3)
+    );
+  }
+
+  getPosts(): Observable<any> {
+    return this.http.get(this.apiURL + '/posts/').pipe(
+      retry(3)
+    );
+  }
+  createPost(post): Observable<any> {
+    return this.http.post(this.apiURL + '/posts', post, this.httpOptions).pipe(
+      retry(3)
+    );
+  }
+
+  updatePost(id, post): Observable<any> {
+    return this.http.put(this.apiURL + '/posts/' + id, post, this.httpOptions).pipe(
+      retry(3));
+  }
+  deletePost(id): Observable<any> {
+    return this.http.delete(this.apiURL + '/posts/' + id, this.httpOptions);
+  }
+
+
+
 }
